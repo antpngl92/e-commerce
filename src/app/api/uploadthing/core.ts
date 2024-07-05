f57902c62dc5328202b8963b86a8946deb2f2554
Anton <nyagolov.anton@gmail.com>
2024-07-05 13:21:27 +0300

    Set create/update image on file upload complete

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,5 +1,7 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { z } from "zod";
+import sharp from "sharp";
+import { db } from "@db";
 
 const f = createUploadthing();
 
@@ -15,7 +17,40 @@ export const ourFileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
       const { configId } = metadata.input;
-      return { configId };
+
+      // Fetch Image
+      const res = await fetch(file.url);
+      //Convert it to buffer
+      const buffer = await res.arrayBuffer();
+      // Grab image metadata
+      const imgMetadata = await sharp(buffer).metadata();
+      // Grab image width and height
+      const { width, height } = imgMetadata;
+
+      if (!configId) {
+        // This is executed on new image upload at Step 1 - create new configuration in DB
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        });
+
+        return { configId: configuration.id };
+      } else {
+        // This is executed on existing image edit at Step 2 - update configuration in DB
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        });
+
+        return { configId: updatedConfiguration.id };
+      }
     }),
 } satisfies FileRouter;
 
