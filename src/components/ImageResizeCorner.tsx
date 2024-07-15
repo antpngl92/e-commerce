faf4c1d61c3df16af8b6613403fbad7c0307b337
Anton <nyagolov.anton@gmail.com>
2024-07-15 17:14:45 +0300

    Create ImageResizeCorner component

---
+++ src/components/ImageResizeCorner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImageResizeCorner = () => {
+  return (
+    <div className='size-5 rounded-full shadow border bg-white border-zinc-200 transition hover:bg-primary' />
+  )
+}
+
+export default ImageResizeCorner
