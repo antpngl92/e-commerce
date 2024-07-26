f3ec42ad01dd2d4042260f5b935a8e4eb572fb32
Anton <nyagolov.anton@gmail.com>
2024-07-26 15:30:27 +0300

    Refactor DesignConfigurator  styles

--- src/components/DesignConfigurator.tsx
+++ src/components/DesignConfigurator.tsx
@@ -146,7 +146,7 @@ const DesignConfigurator = ({
   }
   
   return (
-    <div className="relative mt-20 grid grid-col-1 lg:grid-cols-3 mb-20 pb-20">
+    <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
       <div ref={containerRef} className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
         <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
           <AspectRatio
@@ -211,9 +211,9 @@ const DesignConfigurator = ({
         </Rnd>
       </div>
 
-      <div className="h-[37.5rem] w-full col-span-full  lg:col-span-1 flex flex-col bg-white">
+      <div className="h-[37.5rem] w-full col-span-full lg:col-span-1 flex flex-col bg-white">
         <ScrollArea className="relative flex-1 overflow-auto">
-          <div area-hidden="true" className="absolute z-10 index-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none"/>
+          <div area-hidden="true" className="absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none"/>
         
           <div className="px-8 pb-12 pt-8">
             <h2 className="tracking-tight font-bold text-3xl">Customize your case</h2>
